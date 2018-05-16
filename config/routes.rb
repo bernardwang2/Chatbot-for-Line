38dac42ee0d3a74e9d62bce75704522378eed468
Bernie <bernw7214604@gmail.com>
2018-05-16 16:45:20 -0700

    hi

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :keyword_mappings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/kamigo/eat', to: 'kamigo#eat'
